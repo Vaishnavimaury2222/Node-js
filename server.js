eb7bd40cb9e9f7a2b1a64a060d0a686a6c70af25
champaksworldcreate <champaksworld@gmail.com>
2023-05-10 08:10:52 +0530

    ejs using get parameters

--- server.js
+++ server.js
@@ -2,17 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Define the list of products
-const products = [
-  { name: 'Product 1', price: 10 },
-  { name: 'Product 2', price: 20 },
-  { name: 'Product 3', price: 30 }
-];
-
-// Define a route to render the products page
-app.get('/', (req, res) => {
-  // Render the products page using EJS
-  res.render('products.ejs', { products });
+// Define a route to render the template with GET parameters
+app.get('/hello', (req, res) => {
+  const name = req.query.name || 'world'; // Get the "name" parameter from the query string
+  res.render('hello.ejs', { name }); // Pass the "name" parameter to the template
 });
 
 // Start the server
