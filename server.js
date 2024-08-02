87396338a340b3f7e945cde39852531cb91aac39
champaksworldcreate <champaksworld@gmail.com>
2024-08-02 22:40:31 +0530

    Login Logout support to react.

--- server.js
+++ server.js
@@ -1,71 +1,13 @@
-const mysql = require('mysql');
 const express = require('express');
+const cors = require('cors')
 const app = express();
-const port = 3000;
 
+app.use(cors());
 
-
-
-
-
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'node'
+app.use('/login', (req, res) => {
+  res.send({
+    token: 'test123'
+  });
 });
-connection.connect();
 
-
-
-
-
-
-
-
-
-app.get('/select', (req, res) => {
-
-  res.send("select");
-});
-app.get('/delete', (req, res) => {
-
-  res.send("delete");
-});
-app.get('/update', (req, res) => {
-  console.log(req.query);
-  const { bookname, author, genre ,id} = req.query;
-  console.log(req.query);
-  const sql = 'update  books set bookname=?, author=?, genre=? where id=?';
- result= connection.query(sql,[bookname,author,genre,id]);
- console.log(result);
-
-  res.send("update");
-});
-//************************************************************* */
-app.get('/insert', (req, res) => {
-
-
-  const { bookname, author, genre } = req.query;
-  console.log(req.query);
-  const sql = 'INSERT INTO books (bookname, author, genre) VALUES (?, ?, ?)';
-  connection.query(sql, [bookname, author, genre], (err, result) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    res.status(200).send({
-      id: result.insertId, booknamecls
-      , author, genre
-    });
-  })
-}
-
-  
-);
-//********************************************************************* */
-
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
